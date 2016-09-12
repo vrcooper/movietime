456a7e0bafcb836b10d14196da6a1f318c5723c6
Virginia <vcooper_us@yahoo.com>
2016-09-12 11:53:53 -0700

    Changed model relationships and successfully displayed images.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,3 @@
 class Genre < ActiveRecord::Base
-  belongs_to :movie
+  has_and_belongs_to_many :movie
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
+  has_and_belongs_to_many :genre 
+
   def poster
-    "http://ia.media-imdb.com/images/M/#{poster_url}"
+    "#{poster_url}"
   end
 
   def imdb
